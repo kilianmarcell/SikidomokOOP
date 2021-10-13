1fae07346d76aaeb13843bce1ceb8443e9049752
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-10-13 15:22:51 +0200

    Tesztelés

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -3,6 +3,11 @@ package hu.petrik;
 public class Main {
 
     public static void main(String[] args) {
-
+        Sikidom s1 = new Teglalap(1, 2);
+        System.out.println(((Teglalap)s1).getA()); // == s1.getA();
+        Teglalap t1 = new Teglalap(2, 4);
+        System.out.println(t1.getA());
+        System.out.println(s1);
+        System.out.println(t1);
     }
 }
